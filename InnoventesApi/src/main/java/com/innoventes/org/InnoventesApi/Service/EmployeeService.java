ac081357995eca0cfaeb2577042da95aa62dbe61
Arun <arun@gocoop.com>
2020-01-26 16:38:04 +0530

    Innoventes api task

---
+++ InnoventesApi/src/main/java/com/innoventes/org/InnoventesApi/Service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.innoventes.org.InnoventesApi.Service;
+
+import java.util.List;
+
+import com.innoventes.org.InnoventesApi.Entity.Employee;
+
+import javassist.NotFoundException;
+
+public interface EmployeeService {
+
+	Employee createEmployee(Employee employee);
+
+	Employee updateEmployee(Long id, Employee employee) throws NotFoundException;
+	
+	Employee findByEmployeeId(Long id) throws NotFoundException;
+
+	List<Employee> getAll();
+
+	Employee deleteEmployee(Long id) throws NotFoundException;
+
+
+
+}
